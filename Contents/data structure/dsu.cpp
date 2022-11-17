c5dc3db47faf33973ae1615d386f08608447a854
Kevin051596 <yellow0515car@gmail.com>
2022-11-18 00:04:48 +0800

    add: .vimrc, dsu, suffix array, lcp

---
+++ Contents/data structure/dsu.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> dsu, rk;
+
+void initDSU(int n){
+    dsu.resize(n);
+    rk.resize(n);
+    for(int i = 0; i < n; i++) dsu[i] = i, rk[i] = 1;
+}
+
+int findDSU(int x){
+    if(dsu[x] == x) return x;
+    dsu[x] = findDSU(dsu[x]);
+    return dsu[x];
+}
+
+void unionDSU(int a, int b){
+    int pa = findDSU(a), pb = findDSU(b);
+    if(rk[pa] > rk[pb]) swap(pa, pb);
+    if(rk[pa] == rk[pb]) rk[pb]++;
+    dsu[pa] = pb;
+}
