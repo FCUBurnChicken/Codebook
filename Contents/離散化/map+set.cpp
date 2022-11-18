2447d3278bd85bfa1665d87454804e24115250a4
Vincent0913 <b5851317@gmail.com>
2022-11-18 16:27:05 +0800

    iscretization

---
+++ Contents/離散化/map+set.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> a = {1561, 777, 89898, 5}; // -> {3, 2, 4, 1}
+
+    int now = 1;
+    map<int, int>mp;
+    set<int> ms;
+
+    for(int i:a)
+    {
+        ms.insert(i);
+    }
+
+    for(int i:ms)
+    {
+        mp[i] = now++;
+    }
+
+    for(int i:a)
+    {
+        cout << mp[i] << "\n";
+    }
+
+    return 0;
+}
