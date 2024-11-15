1482338200624e2a60e72f81f6ae66b5f73bbaf3
YJK0805 <jerryko103@gmail.com>
2024-11-15 20:39:54 +0800

    compile codebook

--- Contents/離散化/map+set.cpp
+++ Contents/離散化/map+set.cpp
@@ -1,26 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int main(){
     vector<int> a = {1561, 777, 89898, 5}; // -> {3, 2, 4, 1}
 
     int now = 1;
     map<int, int>mp;
     set<int> ms;
 
-    for(int i:a)
-    {
+    for(int i:a){
         ms.insert(i);
     }
 
-    for(int i:ms)
-    {
+    for(int i:ms){
         mp[i] = now++;
     }
 
-    for(int i:a)
-    {
+    for(int i:a){
         cout << mp[i] << "\n";
     }
 
